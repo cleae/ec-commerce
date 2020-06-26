3cca1b8d45c25ac4b5277bb07da1b1eee4feeba4
tl <320740231@qq.com>
2020-06-27 00:21:32 +0800

    dependcy init

---
+++ ec-commerce-service/src/test/java/com/tl/service/EcCommerceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tl.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcCommerceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
